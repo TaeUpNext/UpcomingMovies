2491eaab869cc932f1797b9b57a182797e621fee
Taeupnext <armontaeh17@gmail.com>
2022-07-15 17:56:56 -0400

    movie year, name and info

--- main.py
+++ main.py
@@ -7,14 +7,23 @@ html_site = requests.get('https://www.imdb.com/calendar/').text #the site were g
 
 soup = BeautifulSoup(html_site, 'lxml')
 
-movies = soup.find_all('div', id =re.compile('^main'))
-
-for movie in movies:
-    movie_name = movies.a.text
-    release_date = movies.h4.text
-    movie_info = movies.a['href']
-    print(f'Movie Name: {movie_name}')
-    print(f'Release Date: {release_date}')
-    print(f'Movie Info: https://www.imdb.com/{movie_info}')
 
 
+#movies = soup.find_all('div', id=re.compile('^main'))
+moviesDiv = soup.find('div', id = 'main')
+moviesUL = moviesDiv.find_all('ul')
+
+moviesReleaseDate = moviesDiv.find_all('h4')
+
+
+
+for movie in moviesUL:
+        movie_name = movie.find('a').text
+        movie_moreInfo = movie.a['href']
+
+        movieInfoSite = requests.get(f'https://www.imdb.com/{movie_moreInfo}').text
+        infoSoup =  BeautifulSoup(movieInfoSite, 'lxml')
+        movieRD = infoSoup.find('span', class_ = 'sc-8c396aa2-2 itZqyK').text.replace(' ','')
+        print(f'Movie Release Year: {movieRD}')
+        print(f'Movie Name: {movie_name}')
+        print(f'More Info: https://www.imdb.com/{movie_moreInfo}\n')
